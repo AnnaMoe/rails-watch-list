5624d4d6b690d0e2fc0562dda707f8154fbe8793
Anna Moelling <anna.moelling@googlemail.com>
2021-04-18 16:31:32 +0200

    styling with bootstrap

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,11 @@
 class ListsController < ApplicationController
+  before_action :find_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
   end
 
-  def show
-    @list = List.find(params[:id])
-  end
+  def show; end
 
   def new
     @list = List.new
@@ -21,7 +20,16 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
+
+  def find_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
